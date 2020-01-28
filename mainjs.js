697299c28e6fbb30610d51e0ef27237072865310
Walter Hoerman <whoerman@gmail.com>
2020-01-28 16:41:00 -0500

    working need to recude amount of code

--- mainjs.js
+++ mainjs.js
@@ -13,9 +13,9 @@ let $appt15 = $("#slot15in");
 let $appt16 = $("#slot16in");
 let $appt17 = $("#slot17in");
 
+
 //getting the current hour for shading
 let currentHour = parseInt(moment().format('H'));
-console.log(currentHour);
 switch (currentHour) {
     case 9:
         $appt9.css("background-color", "#ff2626");
@@ -118,6 +118,22 @@ switch (currentHour) {
 
 }
 
+//checking the local storage for info
+function getStorage() {
+    var backTransfer = JSON.parse(localStorage.getItem('calendarObj'));
+    calendarObj = backTransfer;
+    $appt9.text(calendarObj.slot9);
+    $appt10.text(calendarObj.slot10);
+    $appt11.text(calendarObj.slot11);
+    $appt12.text(calendarObj.slot12);
+    $appt13.text(calendarObj.slot13);
+    $appt14.text(calendarObj.slot14);
+    $appt15.text(calendarObj.slot15);
+    $appt16.text(calendarObj.slot16);
+    $appt17.text(calendarObj.slot17);
+}
+
+getStorage();
 
 
 
@@ -126,67 +142,131 @@ $(document).ready(function () {
     $("#btn9").on("click", function () {
         calendarObj.slot9 = $appt9.val();
         pushStorage();
-        refeshAppointments();
+        refreshAppointments();
     });
+
+    $("#btn9e").on("click", function () {
+        calendarObj.slot9 = "";
+        console.log(calendarObj);
+        pushStorage();
+        refreshAppointments();
+    });
+
     $("#btn10").on("click", function () {
         calendarObj.slot10 = $appt10.val();
         pushStorage();
-        refeshAppointments();
-
+        refreshAppointments();
     });
+
+    $("#btn10e").on("click", function () {
+        calendarObj.slot10 = "";
+        console.log(calendarObj);
+        pushStorage();
+        refreshAppointments();
+    });
+
     $("#btn11").on("click", function () {
         calendarObj.slot11 = $appt11.val();
         pushStorage();
-        refeshAppointments();
-
+        refreshAppointments();
     });
+
+    $("#btn11e").on("click", function () {
+        calendarObj.slot11 = "";
+        console.log(calendarObj);
+        pushStorage();
+        refreshAppointments();
+    });
+
     $("#btn12").on("click", function () {
         calendarObj.slot12 = $appt12.val();
         pushStorage();
-        refeshAppointments();
+        refreshAppointments();
 
     });
+
+    $("#btn12e").on("click", function () {
+        calendarObj.slot12 = "";
+        console.log(calendarObj);
+        pushStorage();
+        refreshAppointments();
+    });
+
     $("#btn13").on("click", function () {
         calendarObj.slot13 = $appt13.val();
         pushStorage();
-        refeshAppointments();
+        refreshAppointments();
+    });
+
+    $("#btn13e").on("click", function () {
+        calendarObj.slot13 = "";
+        console.log(calendarObj);
+        pushStorage();
+        refreshAppointments();
+
 
     });
     $("#btn14").on("click", function () {
         calendarObj.slot14 = $appt14.val();
         pushStorage();
-        refeshAppointments();
-
+        refreshAppointments();
     });
+
+    $("#btn14e").on("click", function () {
+        calendarObj.slot14 = "";
+        console.log(calendarObj);
+        pushStorage();
+        refreshAppointments();
+    });
+
+
     $("#btn15").on("click", function () {
         calendarObj.slot15 = $appt15.val();
         pushStorage();
-        refeshAppointments();
+        refreshAppointments();
 
     });
+
+    $("#btn15e").on("click", function () {
+        calendarObj.slot15 = "";
+        console.log(calendarObj);
+        pushStorage();
+        refreshAppointments();
+    });
+
     $("#btn16").on("click", function () {
         calendarObj.slot16 = $appt16.val();
         pushStorage();
-        refeshAppointments();
+        refreshAppointments();
 
     });
+
+    $("#btn16e").on("click", function () {
+        calendarObj.slot16 = "";
+        console.log(calendarObj);
+        pushStorage();
+        refreshAppointments();
+    });
+
     $("#btn17").on("click", function () {
         calendarObj.slot17 = $appt17.val();
         pushStorage();
-        refeshAppointments();
+        refreshAppointments();
 
+    });
+
+    $("#btn17e").on("click", function () {
+        calendarObj.slot17 = "";
+        console.log(calendarObj);
+        pushStorage();
+        refreshAppointments();
     });
 
     function pushStorage() {
         localStorage.setItem('calendarObj', JSON.stringify(calendarObj));
-        var backTransfer = JSON.parse(localStorage.getItem('calendarObj'));
-        calendarObj = backTransfer;
     }
 
-
-
-
-    function refeshAppointments() {
+    function refreshAppointments() {
         console.log(calendarObj);
         $appt9.text(calendarObj.slot9);
         $appt10.text(calendarObj.slot10);
