d03460594bc1987baa0b9ce69f5b24b812d30eb3
Walter Hoerman <whoerman@gmail.com>
2020-01-28 20:55:38 -0500

    still adding new pages

--- datajs.js
+++ datajs.js
@@ -23,3 +23,16 @@ let calendarObjplus1  = {
     slot16: "",
     slot17: ""
 }
+
+let calendarObjminus1  = {
+    date: dateYesterday,
+    slot9: "",
+    slot10: "",
+    slot11: "",
+    slot12: "",
+    slot13: "",
+    slot14: "",
+    slot15: "",
+    slot16: "",
+    slot17: ""
+}
